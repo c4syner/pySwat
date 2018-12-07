a489af6cdca35a8c1f8ab26adc347397d6d93396
Cliff Syner <33703995+c4syner@users.noreply.github.com>
2018-12-07 10:09:37 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'stonesthrow',         # How you named your package folder (MyLib)
   packages = ['stonesthrow'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A rudimentary peer-to-peer API for sending data, easily.',   # Give a short description about your library
   author = 'Cliff Syner',                   # Type in your name
   url = 'https://github.com/c4syner/Stonesthrow',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/c4syner/Stonesthrow/archive/v0.1-alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/c4syner/Stonesthrow/archive/v0.2-alpha.tar.gz',    # I explain this later on
   keywords = ['API', 'DATA', 'NETWORKING'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
