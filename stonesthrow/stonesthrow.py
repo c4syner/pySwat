a6e202fef66aee42ee7b7c5c172e81cf5bb7a0e7
Cliff Syner <33703995+c4syner@users.noreply.github.com>
2019-01-11 18:27:32 -0600

    Update stonesthrow.py

--- stonesthrow/stonesthrow.py
+++ stonesthrow/stonesthrow.py
@@ -1,38 +1,23 @@
 import requests
 import json
 class Stonesthrow:
-  def getData(address, checkDupli = False): #Name of thing to get data from, check if data is duplicate from previous defaults to False
+  def getData(address, itemIndex): #Name of thing to get data from
     #checks for data for a certain address or "thing" then makes sure it has not been already sent
     curr = 1
-    def updateInfo():
+    def updateInfo(serv, itemDex):
       myUrl = "https://dweet.io/get/latest/dweet/for/" + serv
       myDat = requests.get(myUrl)
       thisJson = json.loads(myDat.text)
       try:
-        myMess= thisJson["with"][0]["content"]["myData"]
+        myMess= thisJson["with"][0]["content"][itemDex]
       except:
         pass
       try:
         return myMess
       except:
         pass
-    serv = address
-    if(checkDupli == True):
-      if curr == 1:
-        lastOp = updateInfo()
-        curr = curr + 1
-      currOp = updateInfo()
-      if(currOp == None):
-        pass
-      else:
-        if(lastOp == currOp):
-          pass
-        else:
-          return currOp
-        lastOp = currOp
-    else:
-      thisData = updateInfo()
-      return thisData
-  def sendData(address, data):
-      url = "https://dweet.io/dweet/for/" + address + "?myData="+ data
+    thisData = updateInfo(address, itemIndex)
+    return thisData
+  def sendData(address, data, itemIndex):
+      url = "https://dweet.io/dweet/for/" + address + "?" + itemIndex+ "="+ data
       requests.get(url)
