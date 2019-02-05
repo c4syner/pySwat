fc3a8ed960fbe01030dbea55156981ca2852c946
Cliff Syner <33703995+c4syner@users.noreply.github.com>
2019-02-05 09:16:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pySwat',         # How you named your package folder (MyLib)
   packages = ['pySwat'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple script to automatically search Stack Overflow for coding errors',   # Give a short description about your library
   author = 'Cliff Syner',                   # Type in your name
   url = 'https://github.com/c4syner/pySwat',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/c4syner/pySwat/archive/v4.0-alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/c4syner/pySwat/archive/v5.0-alpha.tar.gz',    # I explain this later on
   keywords = ['API', 'DEBUGGING', 'PROGRAMMING'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'gsearch',
